23c8a5f07aac149e386ef2e96b49c6a1656b61fc
ClaraZb <czavaronibenoit@udesa.edu.ar>
2025-03-25 20:24:10 -0300

    manejo los errores en el main

--- Ejercicio 1/ejercicio1.cpp
+++ Ejercicio 1/ejercicio1.cpp
@@ -2,63 +2,104 @@
 #include "Tiempo.h"
 
 int main(){
+    //Inicializo parametros
+    int hora; int min; int seg; string momto; int res;
+
     //Inicializo sin parametros
     Tiempo tiempo0;
     cout << "Testeo sin parametros" << endl;
     tiempo0.imprimir();
 
-    //Inicializo con la hora
-    int hora;
-    cout << "Testeo con la hora" << endl;
-    cout << "Ingresar hora: ";
-    cin >> hora;
-    Tiempo tiempo1(hora);
-    tiempo1.imprimir();
+    //Inicializo con la hora (con manejo de error)
+    res = 1;
+    while (res == 1){
+        cout << "Testeo con la hora" << endl;
+        cout << "Ingresar hora: ";
+        cin >> hora;
+        try{
+            Tiempo tiempo1(hora);
+            tiempo1.imprimir();
+            res = 0;
+        }
+        catch(const invalid_argument& e){
+            cout << "Error en el ingreso de los datos" << endl;
+            cout << "Quieres ingresar un numero nuevamente? (responder con 0 o 1)" << endl;
+            cin >> res;
+        }
+    }
 
-    //Inicializando horas y mins
-    int min;
-    cout << "Testeo con la hora y los minutos" << endl;
-    cout << "Ingresar hora y minutos: ";
-    cin >> hora >> min;
-    Tiempo tiempo2(hora, min);
-    tiempo2.imprimir();
+    //Inicializando horas y mins (con manejo de error)
+    res = 1;
+    while (res == 1){
+        int hora;
+        cout << "Testeo con la hora y los minutos" << endl;
+        cout << "Ingresar hora y minutos: ";
+        cin >> hora >> min;
+        try{
+            Tiempo tiempo2(hora, min);
+            tiempo2.imprimir();
+            res = 0;
+        }
+        catch(const invalid_argument& e){
+            cout << "Error en el ingreso de los datos" << endl;
+            cout << "Quieres ingresar un numero nuevamente? (responder con 0 o 1)" << endl;
+            cin >> res;
+        }
+    }
 
     //Inicializando horas, mins y segs
-    int seg;
-    cout << "Testeo con la hora, los minutos y los segundos" << endl;
-    cout << "Ingresar hora, minutos y segundos: ";
-    cin >> hora >> min >> seg;
-    Tiempo tiempo3(hora, min, seg);
-    tiempo3.imprimir();
+    res = 1;
+    while (res == 1){
+        int hora;
+        cout << "Testeo con la hora, los minutos y los segundos" << endl;
+        cout << "Ingresar hora, minutos y segundos: ";
+        cin >> hora >> min >> seg;
+        try{
+            Tiempo tiempo3(hora, min, seg);
+            tiempo3.imprimir();
+            res = 0;
+        }
+        catch(const invalid_argument& e){
+            cout << "Error en el ingreso de los datos" << endl;
+            cout << "Quieres ingresar un numero nuevamente? (responder con 0 o 1)" << endl;
+            cin >> res;
+        }
+    }
 
     //Inicializando horas, mins, segundos y momento del dia
-    string momto;
-    cout << "Testeo con la hora, los minutos, los segundos y el momento del dia" << endl;
-    cout << "Ingresar hora, minutos, segundos y momento del dia (a.m o p.m): ";
-    cin >> hora >> min >> seg >> momto;
-    Tiempo tiempo4(hora, min, seg, momto);
-    tiempo4.imprimir();
-
-    //Inicializando con errores:
-    cout << "Testeo con errores" << endl;
-    Tiempo tiempo5(30);
-    Tiempo tiempo6(10, 61);
-    Tiempo tiempo7(10, 59, 71);
-    Tiempo tiempo8(10, 59, 48, "t.m");
+    res = 1;
+    while (res == 1){
+        int hora;
+        cout << "Testeo con la hora, los minutos, los segundos y el momento del dia" << endl;
+        cout << "Ingresar hora, minutos, segundos y momento del dia (a.m o p.m): ";
+        cin >> hora >> min >> seg >> momto;
+        try{
+            Tiempo tiempo4(hora, min, seg, momto);
+            tiempo4.imprimir();
+            res = 0;
+        }
+        catch(const invalid_argument& e){
+            cout << "Error en el ingreso de los datos" << endl;
+            cout << "Quieres ingresar un numero nuevamente? (responder con 0 o 1)" << endl;
+            cin >> res;
+        }
+    }
 
     //Testeando modificacion de los atributos
     cout << "Testeo modificacion de atributos" << endl;
-    tiempo4.hora = 4;
-    tiempo4.min = 5;
-    tiempo4.seg = 40;
-    tiempo4.momto = "a.m";
+    Tiempo tiempo5(10, 54, 30, "a.m");
+    tiempo5.hora = 4;
+    tiempo5.min = 5;
+    tiempo5.seg = 40;
+    tiempo5.momto = "a.m";
 
-    cout << tiempo4.hora << endl;
-    cout << tiempo4.min << endl;
-    cout << tiempo4.seg << endl;
-    cout << tiempo4.momto << endl;
+    cout << tiempo5.hora << endl;
+    cout << tiempo5.min << endl;
+    cout << tiempo5.seg << endl;
+    cout << tiempo5.momto << endl;
 
     return 0;
 
-    //implementalo con try y catch
+    //testear ajustar formato
+    //chequear valores validos de la hora
 }
